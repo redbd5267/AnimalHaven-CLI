71ab36e5beef7c458a6ebda1d2ed0ab81f4de43c
Fatama Rahman <fatamarahman@gmail.com>
2018-11-14 04:28:57 +0000

    more wip on cli workflow

--- lib/eventbrite/cli.rb
+++ lib/eventbrite/cli.rb
@@ -15,15 +15,20 @@ class Eventbrite::CLI
   end
   
   def menu
-    puts "Enter the number of the event you'd like more info on, or type exit:"
     input = nil
     while input != "exit"
+      puts "Enter the number of the event you'd like more info on, or type list to see the list of events, or type exit:"
       input = gets.strip.downcase
       case input
       when "1"
         puts "more info on event 1"
       when "2" 
         puts "more info on event 2"
+      when "list"
+        list_events
+      else 
+        puts "Not sure, pick again from the list below"
+        list_events
       end
     end
   end
